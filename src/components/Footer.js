252c5ddedcb11ffebb703433aee6eb03e9cd57d5
Darian Sharma <dariansharma@darians-mbp.lan>
2021-05-04 14:46:12 -0500

    added icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,11 @@ export const Footer = () => {
   return (
     <footer>
       <h4>Thanks</h4>
+      <a
+        href="https://www.facebook.com/profile.php?id=100010160056418"
+        className="fa fa-facebook"
+      ></a>
+      <a href="https://twitter.com/dariansharma" className="fa fa-twitter"></a>
     </footer>
   );
 };
