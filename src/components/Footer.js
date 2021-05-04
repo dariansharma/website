d305abe2a604e1aa10f019da5397398112a082a9
Darian Sharma <dariansharma@darians-mbp.lan>
2021-05-04 15:47:33 -0500

    changed footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,8 @@ import React from "react";
 export const Footer = () => {
   return (
     <footer>
-      <h4>Thanks</h4>
-      <a
-        href="https://www.facebook.com/profile.php?id=100010160056418"
-        className="fa fa-facebook"
-      >
-        facebook
-      </a>
-      <a href="https://twitter.com/dariansharma" className="fa fa-twitter">
-        twitter
+      <a href="https://github.com/dariansharma" className="fa fa-github">
+        ""
       </a>
     </footer>
   );
