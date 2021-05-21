343f862d097eb403efa207fa3054038326782199
Darian Sharma <dariansharma@gmail.com>
2021-05-21 00:49:50 -0500

    added emoji shop project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,13 @@ export const Projects = () => {
   return (
     <section className="projects">
       <article>
+        <a href="https://emojishop.netlify.app/">Emoji Shop</a>
+        <p>
+          Learning more React and React Router. Example web shop with different
+          rendered content and urls.
+        </p>
+      </article>
+      <article>
         <a href="https://todo-app-ce094.web.app">Firebase Todo App</a>
         <p>
           Learning basic React components, state, props, form input, form
